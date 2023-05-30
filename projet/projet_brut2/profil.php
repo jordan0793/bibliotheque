d2aa050a74d5f0c381ea300c57636926674d00ed
gosseau1993 <jordanafpa@gmail.com>
2023-05-30 15:40:57 +0200

    push8

--- projet/projet_brut2/profil.php
+++ projet/projet_brut2/profil.php
@@ -9,21 +9,21 @@ include "header.php";
 <main class="container">
     <section class="row">
         <h1 class='my-2'>Profil de <?= ucfirst($_SESSION["user"]["pseudo"]) ?></h1>
-        <p>Pseudo :<?= ucfirst($_SESSION["user"]["pseudo"]) ?></p>
-        <p>Email :<?= $_SESSION["user"]["email"] ?></p>
+        <p class="text">Pseudo :<?= ucfirst($_SESSION["user"]["pseudo"]) ?></p>
+        <p class="text">Email :<?= $_SESSION["user"]["email"] ?></p>
         <?php
         if ($_SESSION["user"]["roles"] === '["ROLE_USER"]["ROLE_ADMIN"]') {
             $roleAttribue = "Administrateur";
 
         ?>
-            <p>Role :<?= $roleAttribue ?></p>
+            <p class="text">Role :<?= $roleAttribue ?></p>
         <?php
         } else {
             $roleAttribue = "Utilisateur";
 
 
         ?>
-            <p>Role :<?= $roleAttribue ?></p>
+            <p class="text">Role :<?= $roleAttribue ?></p>
 
 
         <?php
@@ -39,20 +39,10 @@ include "header.php";
     }
 
     h1,
-    p {
+    .text {
         padding-top: 5%;
     }
 </style>
-
-
-
-<!-- Récupération des données de Connexion dans la BDD -->
-
-
-
-
-
-
 
 <?php
 //on inclu le footer 
